79fb58462045c5792927f88d665e4d2ffeba99ed
焦国栋 <jgdnew21@aliyun.com>
2026-05-20 15:44:59 +0800

    update

--- app/config.py
+++ app/config.py
@@ -23,17 +23,13 @@ class Settings:
     - SNAPSHOT_DIR: confirmation snapshots used to enforce prepare/confirm consistency.
     """
 
-    erpnext_base_url: str = os.getenv("ERPNEXT_BASE_URL", "http://site1.localhost:8080")
+    erpnext_base_url: str = os.getenv("ERPNEXT_BASE_URL", "http://hst.localhost:8080")
     erpnext_api_key: str = os.getenv("ERPNEXT_API_KEY", "")
     erpnext_api_secret: str = os.getenv("ERPNEXT_API_SECRET", "")
     erpnext_company: str = os.getenv("ERPNEXT_COMPANY", "")
     default_warehouse_alias: str = os.getenv("DEFAULT_WAREHOUSE_ALIAS", "南宁仓")
-    default_purchase_warehouse: str = os.getenv(
-        "DEFAULT_PURCHASE_WAREHOUSE", os.getenv("DEFAULT_WAREHOUSE_ALIAS", "南宁仓")
-    )
-    default_purchase_supplier: str = os.getenv(
-        "DEFAULT_PURCHASE_SUPPLIER", "市场采购供应商"
-    )
+    default_purchase_warehouse: str = os.getenv("DEFAULT_WAREHOUSE", "南宁华食泰 - 华食泰")
+    default_purchase_supplier: str = os.getenv("DEFAULT_PURCHASE_SUPPLIER", "其它")
     snapshot_dir: str = os.getenv("SNAPSHOT_DIR", "data/snapshots")
     master_data_dir: str = os.getenv("MASTER_DATA_DIR", "data/master_data")
     master_data_cache_db: str = os.getenv(
